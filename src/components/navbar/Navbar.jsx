ea36377e92f340365be7382c8c78a611320b9d70
Min Khant Kyaw <minkhantkyaw@cloudsource.co.jp>
2023-07-03 15:49:37 +0630

    features done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Menu = ()=> (
         <p><a href="#home">Home</a></p>
         <p><a href="#wgpt3">What is GPT3?</a></p>
         <p><a href="#possitilty">OpenAI</a></p>
-        <p><a href="#feature">Case Studies</a></p>
+        <p><a href="#features">Case Studies</a></p>
         <p><a href="#blog">Library</a></p>
     </>
 )
